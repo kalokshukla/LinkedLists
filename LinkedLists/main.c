9857ad20ba1ba01f3f6e4f6a5c7359b718c0ba30
Alok Kumar Shukla <mail@alokkumarshukla.com>
2013-03-07 16:19:35 +0530

    Initial Commit

---
+++ LinkedLists/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LinkedLists
+//
+//  Created by Alok K. Shukla on 07/03/13.
+//  Copyright (c) 2013 Alok Kumar Shukla. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
